55ce225bf079c6029fc9b8c3a8004a8c4defc8b0
b120070 <bisrimustafa0070@gmail.com>
2022-07-08 20:19:53 +0700

    oop

---
+++ oop/oop.java
@@ -0,0 +1,15 @@
+package tugas_oop;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main (String args[]){
+  Scanner sc = new Scanner(System.in);
+   String easy = sc.next();
+
+   if (easy.equalsIgnoreCase("persegi")){
+       persegi persegi = new persegi();
+   }
+    }
+}
+
